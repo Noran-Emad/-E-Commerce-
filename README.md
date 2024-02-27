949e8942a48aacbf50426649214597ca21322aef
Noran-Emad <noren.emad79@gmail.com>
2024-02-27 22:17:23 +0200

    Admin routing is done

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 #  E-Commerce 
   E-Commerce Node Project
-
-Port = 3000
-DBLINK ="mongodb://127.0.0.1:27017/amazon"
--- Routes/Admin.router.js
+++ Routes/Admin.router.js
@@ -3,25 +3,26 @@ const { GetAllProducts, DeleteProduct, AddProduct, EditProduct } = require("../C
 const { GetAllCategory, DeleteCategory, AddCategory, EditCategory } = require("../Controllers/Category.controller")
 const { GetAllUsers } = require("../controllers/Admin.controller")
 const { GetAllOrders } = require("../Controllers/Orders.controller")
+const { isAdmin } = require("../Middleware/admin")
 const router = expree.Router()
 
 /// GET 
-router.get('/users', GetAllUsers)
-router.get('/orders', GetAllOrders)
-router.get('/products', GetAllProducts)
-router.get('/categories', GetAllCategory)
+router.get('/users', isAdmin, GetAllUsers)
+router.get('/orders', isAdmin, GetAllOrders)
+router.get('/products', isAdmin, GetAllProducts)
+router.get('/categories', isAdmin, GetAllCategory)
 
 ///ADD - POST
-router.post('/products/', AddProduct);
-router.post('/categories/', AddCategory);
+router.post('/products/', isAdmin, AddProduct);
+router.post('/categories/', isAdmin, AddCategory);
 
 ///EDIT - PATCH
-router.patch('products/:id',EditProduct);
-router.patch('categories/:id',EditCategory);
+router.patch('/products/:id', isAdmin,EditProduct);
+router.patch('/categories/:id', isAdmin,EditCategory);
 
 ///DELETE
-router.delete('/categories/:id', DeleteCategory);
-router.delete('/products/:id', DeleteProduct);
+router.delete('/products/:id', isAdmin, DeleteProduct);
+router.delete('/categories/:id', isAdmin, DeleteCategory);
 
 module.exports = router;
 
